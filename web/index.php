ec3dce31a9da3c00d52a66df8316a3b4b5b351bf
Daniil Sazhin <daniildr22@gmail.com>
2016-10-27 14:40:23 +0300

    connect bootstrap in demo

--- web/index.php
+++ web/index.php
@@ -11,8 +11,10 @@ use chat\Server;
 <head>
     <meta charset="UTF-8">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
     <script src="js/socketChat.js"></script>
     <script src="js/socketChatDemo.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/socketChatDemo.css">
     <title>Title</title>
 </head>
@@ -22,17 +24,25 @@ use chat\Server;
         <?= Server::fillJavaConstants(); ?>
     });
 </script>
-<div style="float: right;">
-    <div id="users_container" style="width: 160px;">
-        <div class="active">to all</div>
+<div class="row">
+    <div class="col-xs-3">
+        <div>
+            <button class="btn btn-success" onclick="socketChat.open();">open chat</button>
+            <button class="btn btn-danger" onclick="socketChat.close();">close chat</button>
+        </div>
+        <div>
+            <textarea id="socketChat"></textarea>
+        </div>
+        <div>
+            <button class="btn btn-info" onclick="sendMessage();">send message</button>
+        </div>
+    </div>
+    <div class="well col-xs-6" id="dialog_container"></div>
+    <div class="col-xs-3 users_list">
+        <div id="users_container">
+            <div class="active">to all</div>
+        </div>
     </div>
 </div>
-<div id="dialog_container"></div>
-<button onclick="socketChat.open();">open chat</button>
-<button onclick="socketChat.close();">close chat</button>
-<br>
-<textarea id="socketChat"></textarea>
-<br>
-<button onclick="sendMessage();">send message</button>
 </body>
 </html>
